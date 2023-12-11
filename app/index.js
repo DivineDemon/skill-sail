498040ff26977312eb2f99e43cfcb357589c43f5
DivineDemon <supame123@gmail.com>
2023-12-11 19:54:10 +0400

    Home Layout.

--- app/index.js
+++ app/index.js
@@ -1,10 +1,41 @@
-import { View, Text } from "react-native";
+import tw from "twrnc";
+import { useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES } from "../constants";
+import { View, Text, ScrollView, SafeAreaView } from "react-native";
+import {
+  Welcome,
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+} from "../components";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={tw`flex-1 bg-[${COLORS.lightWhite}]`}>
+      <Stack.Screen
+        style={tw`bg-red-500`}
+        options={{
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+          headerTitle: "title",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={tw`flex-1 p-[${SIZES.medium}]`}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
