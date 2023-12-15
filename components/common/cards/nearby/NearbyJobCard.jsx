5628ec6d07c909428da6863c8414190844759b18
DivineDemon <supame123@gmail.com>
2023-12-15 14:49:34 +0400

    Nearby Jobs Done !

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,13 +1,53 @@
-import { View, Text } from 'react-native'
+import tw from "twrnc";
+import { View, Text, Pressable, Image } from "react-native";
 
-import styles from './nearbyjobcard.style'
+import styles from "./nearbyjobcard.style";
+import { checkImageURL } from "../../../../utils";
+import { COLORS, FONT, SIZES } from "../../../../constants";
 
-const NearbyJobCard = () => {
+const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
-  )
-}
+    <Pressable
+      style={tw`flex-1 flex flex-row items-center justify-between p-[${SIZES.medium}px] rounded-[${SIZES.small}px] bg-white shadow-md shadow-[${COLORS.white}]`}
+      onPress={() => handleNavigate(job)}>
+      <Pressable
+        style={tw`w-[50px] h-[50px] bg-[${COLORS.white}] rounded-[${SIZES.medium}] flex items-center justify-center`}>
+        <Image
+          source={{
+            uri: checkImageURL(job.employer_logo)
+              ? job.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          resizeMode="contain"
+          style={tw`w-[70%] h-[70%]`}
+        />
+      </Pressable>
 
-export default NearbyJobCard
+      <View style={tw`flex-1 mx-[${SIZES.medium}px]`}>
+        <Text
+          style={[
+            tw`text-[${SIZES.medium}px] text-[${COLORS.primary}]`,
+            {
+              fontFamily: FONT.bold,
+            },
+          ]}
+          numberOfLines={1}>
+          {job.job_title}
+        </Text>
+        <Text
+          style={[
+            tw`text-[${SIZES.small + 2}px] text-[${
+              COLORS.gray
+            }] mt-[3px] capitalize`,
+            {
+              fontFamily: FONT.regular,
+            },
+          ]}>
+          {job.job_employment_type}
+        </Text>
+      </View>
+    </Pressable>
+  );
+};
+
+export default NearbyJobCard;
